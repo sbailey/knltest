7b72e97b1ed34cdcbc88f3a9eb1e6f33f6a1f1da
Stephen Bailey <StephenBailey@lbl.gov>
2016-12-28 22:27:09 -0800

    adjust max number of batches

--- code/loopsine_mp.py
+++ code/loopsine_mp.py
@@ -3,6 +3,7 @@
 from __future__ import division, print_function
 
 import multiprocessing as mp
+import numpy as np
 from loopsine import loopsine_purepy, loopsine_numpy, loopsine_numba
 from knltest import timeit
 
@@ -12,12 +13,17 @@ loopsine_numpy(2)
 loopsine_numba(2)
 
 n = 10000000
-args = [n,] * mp.cpu_count() * 2
+
+#- Fill up the CPUs with work to do, but don't go crazy
+x = min(6, np.log2(mp.cpu_count() * 2))
+nargs = 2**int(x)
+
+args = [n,] * nargs
 ncpu = 1
 print("# Testing multiprocessing sum(sin(range({})))".format(n))
 print("# Processing {} batches with ncpu processes".format(len(args)))
 print("# method ncpu time     rate")
-while ncpu <= mp.cpu_count():
+while ncpu <= nargs:
     pool = mp.Pool(ncpu)
     t1 = timeit(pool.map, (loopsine_purepy, args))
     t2 = timeit(pool.map, (loopsine_numpy, args))
@@ -32,5 +38,4 @@ while ncpu <= mp.cpu_count():
     print('numba  {:3d} {:8.3f} {:8.3f}'.format(ncpu, t3, r3))
     
     ncpu *= 2
-    
-    
+
