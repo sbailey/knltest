cd87a2e2525af1554888555ee1d14f9f65e2e307
Stephen Bailey <StephenBailey@lbl.gov>
2017-01-14 22:34:08 -0800

    knl update; doesn't work yet

--- code/rrzscan.py
+++ code/rrzscan.py
@@ -8,10 +8,19 @@ if __name__ == '__main__':
     import redrock.test.util
     from knltest import timeit
     
+    wavestep = 2
     if (len(sys.argv) == 1) or (sys.argv[1] == 'laptop'):
-        wavestep = 2
-        redshifts = np.linspace(0.05,1,500)
         nspec = 10
+        redshifts = np.linspace(0.05,1,500)
+        ncpu_list = (1,2,4)
+    elif (sys.argv[1] in ('cori', 'haswell')):
+        nspec = 10
+        redshifts = np.linspace(0.05,1,500)
+        ncpu_list = (1,2,4,8,16,32,64)
+    elif (sys.argv[1] == 'knl'):
+        nspec = 5
+        redshifts = np.linspace(0.05,1,200)
+        ncpu_list = (8,32,64)
     
     template = redrock.test.util.get_template(wavestep=wavestep)
     template.redshifts = np.linspace(0.05,1,500)
@@ -24,12 +33,13 @@ if __name__ == '__main__':
     #- Wake up numba
     redrock.zscan.parallel_calc_zchi2_targets(template.redshifts[0::10], targets[0:1], template)
 
+    ntot = len(template.redshifts) * len(targets)
     print('ncpu nthread  time    rate')
-    for ncpu in (1,2,4):
+    for ncpu in ncpu_list:
         numthreads = max(1, mp.cpu_count() // ncpu)
         args = (template.redshifts, targets, template)
         kwargs = dict(ncpu=ncpu, verbose=False, numthreads=numthreads)
         t = timeit(redrock.zscan.parallel_calc_zchi2_targets, args, kwargs)
-        print('{:3d}    {:3d}    {:.2f}    {:.2f}'.format(ncpu, numthreads, t, len(targets)/t))
+        print('{:3d}    {:3d}    {:.2f}    {:.2f}'.format(ncpu, numthreads, t, ntot/t))
 
     
